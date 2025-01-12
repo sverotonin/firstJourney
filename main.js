9fe495b0e109b1c28e61eac6efa97368bca66ce5
Wilmer <wilmerlapuz@gmail.com>
2025-01-12 22:04:55 +0800

    fixed issue with onclick and replaced it with addEventListener

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ const addBtn = document.getElementById("addBtn");
 const addInpt = document.getElementById("addInpt");
 const todos = document.getElementById("todos");
 
-function addTodo() {
+addBtn.addEventListener("click", function () {
   if (addInpt.value) {
     const todo = document.createElement("li");
     todo.textContent = addInpt.value;
     todos.appendChild(todo);
     addInpt.value = "";
   }
-}
+});
